2c6ac759602c02a8e10a905d2726c7050fc71810
Lucas Henrique <lucasmonte2011@gmail.com>
2025-10-05 15:56:36 -0300

    chore(config): update project configuration files

--- application/config/routes.php
+++ application/config/routes.php
@@ -52,3 +52,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'welcome';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
+
+// Rotas da API de Usuários
+$route['api/users'] = 'api/users/index';
+$route['api/users/(:num)'] = 'api/users/index/$1';
